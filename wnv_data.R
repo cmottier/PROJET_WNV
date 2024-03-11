bebcd8113a3acd1f360b4f24e3bb4ffb80f909de
cmottier <camille.mottier@etu.umontpellier.fr>
2024-03-11 19:55:29 +0100

    Code et résultats pour les noeuds internes

--- wnv_data.R
+++ wnv_data.R
@@ -57,9 +57,14 @@ rec_table <- list(node = seq_len(n_tips + n_nodes))
 
 # Estimateur MB (Mardia)
 MB_ancestral <- mut
-# Allocate locations to the data table (first tips, then nodes)
-rec_table[["location1"]] <- c(dat$lat, rep(MB_ancestral[1], n_nodes))
-rec_table[["location2"]] <- c(dat$long, rep(MB_ancestral[2], n_nodes))
+# # Allocate locations to the data table (first tips, then nodes)
+# rec_table[["location1"]] <- c(dat$lat, rep(MB_ancestral[1], n_nodes))
+# rec_table[["location2"]] <- c(dat$long, rep(MB_ancestral[2], n_nodes))
+
+# Estimateur des noeuds intermédiaires
+X <- Estimateur
+rec_table[["location1"]] <- c(dat$lat, MB_ancestral[1], Estimateur[1:102])
+rec_table[["location2"]] <- c(dat$long, MB_ancestral[2], Estimateur[103:204])
 
 # Format the data to export it
 rec_table <- as_tibble(rec_table)
@@ -67,4 +72,4 @@ tree_tibble <- as_tibble(tree)
 tree_data <- full_join(tree_tibble, rec_table, by = 'node')
 tree_data <- as.treedata(tree_data)
 # Write the extended newick. The resulting file can be read by Evolaps.
-write.beast(tree_data, file = here("results", "tree_with_data_MB.tree"), tree.name = "TREE_MB")
+write.beast(tree_data, file = here("results", "tree_with_data_intermediaires.tree"), tree.name = "TREE_intermediaires")
